7f540ecea16d85859dd10b8b2e7b1a7144cbc7a3
Star <129488313+Bombel1234@users.noreply.github.com>
2025-10-27 15:19:23 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,10 @@ const btn_start = document.getElementById('btn-start');
 btn_start.addEventListener('click', () => {
     // loggedInUserId
     if (localStorage.getItem('loggedInUserId')) {
-        window.location.href = 'htmls/logo.html';
+        window.location.href = 'htmls/registr.html';
     }
     else {
         window.location.href = 'htmls/registr.html';
     }
+
 });
